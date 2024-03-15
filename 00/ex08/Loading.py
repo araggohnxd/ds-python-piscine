e3142b6fce54072c8a04b56453c60ca4b40297f9
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2024-03-15 20:10:33 -0300

    s00e08

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -1,32 +1,58 @@
-import time
-
-
-def format_time(seconds):
-    m, s = divmod(seconds, 60)
-    return f"{int(m):02d}:{int(s):02d}"
-
-
 def ft_tqdm(iterable):
     total = len(iterable)
-    progress = 0
-    start = time.time()
 
     for i, item in enumerate(iterable, start=1):
         yield item
 
-        elapsed = time.time() - start
-        speed = i / elapsed
-        eta = (total - i) / speed
-        progress += 1
-        percent_complete = progress * 100 // total
-
-        str_speed = f"{speed:.2f}it/s"
-        str_time = f"{format_time(elapsed)}<{format_time(eta)}"
-        str_total = f"{item + 1:}/{total}"
-        str_percent = f"{percent_complete:>3}%|{'█' * percent_complete:<100}|"
+        percent = i * 100 // total
+        str_progress = f"{percent:>3}%|{'█' * percent:<100}|"
+        str_total = f"{i}/{total}"
 
         print(
-            f"\r{str_percent} {str_total} [{str_time}, {str_speed}]",
+            f"\r{str_progress} {str_total}",
             end='',
             flush=True
         )
+
+# The code below works more similar to the original function than the above,
+# but uses external libraries, which are forbidden.
+
+# import time
+# import os
+
+
+# def format_time(t):
+#     mins, s = divmod(int(t), 60)
+#     h, m = divmod(mins, 60)
+#     return f'{h:d}:{m:02d}:{s:02d}' if h else f'{m:02d}:{s:02d}'
+
+
+# def ft_tqdm(iterable):
+#     total = len(iterable)
+#     start = time.time()
+#     terminal_width = os.get_terminal_size().columns
+
+#     for i, item in enumerate(iterable, start=1):
+#         yield item
+
+#         elapsed = time.time() - start
+#         speed = i / elapsed
+#         eta = (total - i) / speed
+#         percent = i / total * 100
+
+#         str_percent = f"{percent:3.0f}%|"
+#         str_total = f"| {i:>{len(str(total))}}/{total} "
+#         str_time = f"[{format_time(elapsed)}<{format_time(eta)}, "
+#         str_speed = f"{speed:.2f}it/s]"
+
+#         strs_len = len(str_percent + str_total + str_time + str_speed)
+#         progress_bar_size = terminal_width - strs_len
+#         current_size = int(i / total * progress_bar_size)
+
+#         str_progress = f"{'█' * current_size:<{progress_bar_size}}"
+
+#         print(
+#             f"\r{str_percent}{str_progress}{str_total}{str_time}{str_speed}",
+#             end='',
+#             flush=True
+#         )
